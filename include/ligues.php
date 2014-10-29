433510e60fbd315d08466f0d1a21c7f796fbbc15
Dung <thuydungnguyen275@yahoo.fr>
2014-10-29 15:40:10 +0100

    carousel

--- include/ligues.php
+++ include/ligues.php
@@ -86,58 +86,101 @@
 <div class="modal fade" id="imgCarousel">
   <div class="modal-dialog">
     <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h4 class="modal-title">Photos ligue 1</h4>
-      </div>
-      <div class="modal-body">
-		<div id="imgCarousel" class="carousel slide">
+      	<div class="modal-body">
+		<div id="myCarousel" class="carousel slide">
 			<ol class="carousel-indicators">
-				<li data-target="#imgCarousel" data-slide-to="0" class="active"></li>
-				<li data-target="#imgCarousel" data-slide-to="1"></li>
-				<li data-target="#imgCarousel" data-slide-to="2"></li>
-				<li data-target="#imgCarousel" data-slide-to="3"></li>
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+				<li data-target="#myCarousel" data-slide-to="3"></li>
 			</ol>
 
 			<div class="carousel-inner">
 				<div class="item active">
-					<img src="http://lorempixel.com/1200/900/sports/" alt="" class="img-responsive">
+					<img src="http://lorempicsum.com/simpsons/1254/600/4" alt="simpsons" class="img-responsive">
 					<div class="carousel-caption">
 						<h4>simpsons</h4>
 					</div>
 				</div>
 
 				<div class="item">
-					<img src="http://lorempixel.com/1200/900/cats/" alt="" class="img-responsive">
+					<img src="http://lorempicsum.com/nemo/1254/600/4" alt="simpsons" class="img-responsive">
 					<div class="carousel-caption">
 						<h4>nemo</h4>
 					</div>
 				</div>
 
 				<div class="item">
-					<img src="http://lorempixel.com/1200/900/city/" alt="" class="img-responsive">
+					<img src="http://lorempicsum.com/rio/1254/600/4" alt="simpsons" class="img-responsive">
 					<div class="carousel-caption">
 						<h4>rio</h4>
 					</div>
 				</div>
 
 				<div class="item">
-					<img src="http://lorempixel.com/1200/900/nature/" alt="" class="img-responsive">
+					<img src="http://lorempicsum.com/up/1254/600/4" alt="simpsons" class="img-responsive">
 					<div class="carousel-caption">
 						<h4>up</h4>
 					</div>
 				</div>
 			</div>
-
-			<a href="#imgCarousel" class="carousel-control left" data-slide="prev">
+			<a href="#myCarousel" class="carousel-control left" data-slide="prev">
 				<span class="icon-prev"></span>
 			</a>
 
-			<a href="#imgCarousel" class="carousel-control right" data-slide="next">
+			<a href="#myCarousel" class="carousel-control right" data-slide="next">
 				<span class="icon-next"></span>
 			</a>
-		</div> <!-- end imgCarousel -->
+		</div>
       </div>
     </div>
   </div>
 </div>
+
+
+<div class="container">
+<div id="myCarousel" class="carousel slide">
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+				<li data-target="#myCarousel" data-slide-to="3"></li>
+			</ol>
+
+			<div class="carousel-inner">
+				<div class="item active">
+					<img src="http://lorempicsum.com/simpsons/1254/600/4" alt="simpsons" class="img-responsive">
+					<div class="carousel-caption">
+						<h4>simpsons</h4>
+					</div>
+				</div>
+
+				<div class="item">
+					<img src="http://lorempicsum.com/nemo/1254/600/4" alt="simpsons" class="img-responsive">
+					<div class="carousel-caption">
+						<h4>nemo</h4>
+					</div>
+				</div>
+
+				<div class="item">
+					<img src="http://lorempicsum.com/rio/1254/600/4" alt="simpsons" class="img-responsive">
+					<div class="carousel-caption">
+						<h4>rio</h4>
+					</div>
+				</div>
+
+				<div class="item">
+					<img src="http://lorempicsum.com/up/1254/600/4" alt="simpsons" class="img-responsive">
+					<div class="carousel-caption">
+						<h4>up</h4>
+					</div>
+				</div>
+			</div>
+			<a href="#myCarousel" class="carousel-control left" data-slide="prev">
+				<span class="icon-prev"></span>
+			</a>
+
+			<a href="#myCarousel" class="carousel-control right" data-slide="next">
+				<span class="icon-next"></span>
+			</a>
+		</div> </div>

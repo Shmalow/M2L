7228fb243794ee0f43b464acf5ff42b2fa17ecd3
Dung <thuydungnguyen275@yahoo.fr>
2014-10-29 15:41:20 +0100

    carousel

--- include/ligues.php
+++ include/ligues.php
@@ -81,7 +81,6 @@
 	</div>
 </div>
 
-
 <!--Carousel-->
 <div class="modal fade" id="imgCarousel">
   <div class="modal-dialog">
@@ -136,51 +135,3 @@
     </div>
   </div>
 </div>
-
-
-<div class="container">
-<div id="myCarousel" class="carousel slide">
-			<ol class="carousel-indicators">
-				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-				<li data-target="#myCarousel" data-slide-to="1"></li>
-				<li data-target="#myCarousel" data-slide-to="2"></li>
-				<li data-target="#myCarousel" data-slide-to="3"></li>
-			</ol>
-
-			<div class="carousel-inner">
-				<div class="item active">
-					<img src="http://lorempicsum.com/simpsons/1254/600/4" alt="simpsons" class="img-responsive">
-					<div class="carousel-caption">
-						<h4>simpsons</h4>
-					</div>
-				</div>
-
-				<div class="item">
-					<img src="http://lorempicsum.com/nemo/1254/600/4" alt="simpsons" class="img-responsive">
-					<div class="carousel-caption">
-						<h4>nemo</h4>
-					</div>
-				</div>
-
-				<div class="item">
-					<img src="http://lorempicsum.com/rio/1254/600/4" alt="simpsons" class="img-responsive">
-					<div class="carousel-caption">
-						<h4>rio</h4>
-					</div>
-				</div>
-
-				<div class="item">
-					<img src="http://lorempicsum.com/up/1254/600/4" alt="simpsons" class="img-responsive">
-					<div class="carousel-caption">
-						<h4>up</h4>
-					</div>
-				</div>
-			</div>
-			<a href="#myCarousel" class="carousel-control left" data-slide="prev">
-				<span class="icon-prev"></span>
-			</a>
-
-			<a href="#myCarousel" class="carousel-control right" data-slide="next">
-				<span class="icon-next"></span>
-			</a>
-		</div> </div>
